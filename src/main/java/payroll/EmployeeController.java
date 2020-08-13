44a1316f1a03028b09e4e0533f338cb139830ad9
Yoju Lee <yoyoyoju@gmail.com>
2020-08-13 12:05:59 +0200

    Add Assembler to generate the link

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -17,9 +17,11 @@ import static org.springframework.hateoas.server.mvc.ControllerLinkBuilder.*;
 @RestController
 class EmployeeController {
     private final EmployeeRepository repository;
+    private final EmployeeModelAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/")
@@ -27,20 +29,11 @@ class EmployeeController {
         return "hi";
     }
 
-    /*
-    @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
-    }
-    */
-
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
         List<EntityModel<Employee>> employees = repository.findAll()
             .stream()
-            .map(employee -> EntityModel.of(employee,
-                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+            .map(assembler::toModel)
             .collect(Collectors.toList());
 
         return CollectionModel.of(employees,
@@ -57,18 +50,8 @@ class EmployeeController {
         Employee employee = repository.findById(id) //
             .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, //
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
-
-    /*
-    @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id)
-            .orElseThrow(() -> new EmployeeNotFoundException(id));
-    }
-    */
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
