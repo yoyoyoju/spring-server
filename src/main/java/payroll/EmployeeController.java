b56895d0726ba9ab7e67add16a05783dd582a71e
Yoju Lee <yoyoyoju@gmail.com>
2020-08-13 13:46:07 +0200

    Update PUT method

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -68,6 +68,28 @@ class EmployeeController {
     }
 
     @PutMapping("/employees/{id}")
+    ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee,
+            @PathVariable Long id) {
+        Employee updatedEmployee = repository.findById(id)
+            .map(employee -> {
+                employee.setName(newEmployee.getName());
+                employee.setRole(newEmployee.getRole());
+                return repository.save(employee);
+            })
+            .orElseGet(() -> {
+                newEmployee.setId(id);
+                return repository.save(newEmployee);
+            });
+
+        EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+        return ResponseEntity
+            .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+            .body(entityModel);
+    }
+
+    /*
+    @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
         return repository.findById(id)
             .map(employee -> {
@@ -80,6 +102,7 @@ class EmployeeController {
                 return repository.save(newEmployee);
             });
     }
+    */
 
     @DeleteMapping("/employees/{id}")
     void deleteEmployee(@PathVariable Long id) {
