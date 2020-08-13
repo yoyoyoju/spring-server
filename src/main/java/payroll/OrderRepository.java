9e4944b5dd506f3aab90fde6e6e69cca9277c794
Yoju Lee <yoyoyoju@gmail.com>
2020-08-13 15:27:48 +0200

    Add Orders

---
+++ src/main/java/payroll/OrderRepository.java
@@ -0,0 +1,11 @@
+package payroll;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface OrderRepository extends JpaRepository<Order, Long> {
+    // Creating new instances
+    // Updating existing ones
+    // Deleting
+    // Finding
+
+}
