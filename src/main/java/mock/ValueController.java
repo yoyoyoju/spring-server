89980d05f449dc11212f2baa13d7de5ac4f54669
Yoju Lee <yoyoyoju@gmail.com>
2020-08-16 15:02:02 +0200

    Update CORS www.zzzit.net

--- src/main/java/mock/ValueController.java
+++ src/main/java/mock/ValueController.java
@@ -33,7 +33,7 @@ class ValueController {
         return "hi, it give mock data.";
     }
 
-    @CrossOrigin(origins = "https://zzzit.net")
+    @CrossOrigin(origins = "https://www.zzzit.net")
     @GetMapping("/values")
     CollectionModel<EntityModel<Value>> all() {
         List<EntityModel<Value>> values = repository.findAll()
